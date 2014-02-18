3a86e53c8f58743633d2b0c2ba0f7073e6731668
wrongu <richard.d.lange.13@dartmouth.edu>
2014-02-18 15:41:31 -0500

    further developed Board.py with Node and Wall classes

--- Board.py
+++ Board.py
@@ -6,31 +6,66 @@
 # Date: February 2014
 
 class Node(object):
-	""" A Node is a square on the SIZE x SIZE grid references to neighbors.
-		Neighbors will be removed as walls are placed.
-
-		In terms of implementation, it is essentially a wrapper for a tuple (r, c)
-		with neighbors, connect() and disconnect() functions
+	""" A Node is a square on the SIZE x SIZE grid references to up to 4 adjacent walls.
 	"""
+	NORTH = 0 # row-
+	EAST = 1  # col+
+	SOUTH = 2 # row+
+	WEST = 3  # col-
 
-	def __init__(self, pos=None):
-		self.neighbors = []
+	def __init__(self, pos):
+		self.walls = [None]*4 # NORTH, EAST, SOUTH, WEST
 		self.position = pos
 
-	def __eq__(self, other):
-		return self.position == other.position
+	def wall(self, w, direction):
+		"""Add the given wall to this Node's list of adjacent walls"""
+		self.walls[direction] = w
+
+	def unwall(self, direction):
+		"""remove the wall (if it exists) indicated by the given direction"""
+		self.walls[direction] = None
+
+	def has_wall(self, direction):
+		return self.walls[direction] != None
+
+	@classmethod
+	def notate(cls, row_col):
+		"""return the string notation of the given (row, column) tuple as described in Quoridor.py"""
+		return "%d%c" % (row_col[0]+1, chr(ord('a') + row_col[1]))
+
+	@classmethod
+	def parse(cls, notate):
+		"""the inverse of notate... takes a notation string ('1a' - '9i') and returns the tuple (row, column) ((0,0) - (8,8))"""
+		return (int(notate[0] - 1), ord(notate[1]) - ord('a'))
 
 	def __str__(self):
-		return str(self.position)
+		return Node.notate(self.position)
 
-	def connect(self, other):
-		self.neighbors.append(other)
+class Wall(object):
+	"""A Wall spans 2 grid spaces and can be either vertical or horizontal.
+	"""
 
-	def disconnect(self, other):
-		if other in self.neighbors:
-			self.neighbors.remove(other)
-		else:
-			raise Exception("cannot remove %s from %s.. not connected" % (str(self), str(other)))
+	VERTICAL = 'V'
+	HORIZONTAL = 'H'
+
+	def __init__(self, topleft, orient):
+		self.orientation = orient
+		self.position = topleft
+
+	@classmethod
+	def notate(cls, topleft, orient):
+		"""return the string notation of this Wall as described in Quoridor.py"""
+		return "%c%s" % (orient, Node.notate(topleft))
+
+	@classmethod
+	def parse(cls, notate):
+		"""the inverse of notate... take a notation string and return a wall object"""
+		orient = notate[0]
+		pos = Node.parse(notate[1:])
+		return Wall(pos, orient)
+
+	def __str__(self):
+		return Wall.notate(self.position, self.orientation)
 
 class Board(object):
 
@@ -38,22 +73,53 @@ class Board(object):
 
 	def __init__(self):
 		self.grid = Board.__create_grid()
+		self.walls = []
+
+	def __neighbors(self, pos):
+		neighbors = {
+			Node.NORTH : (pos[0]-1, pos[1]) if pos[0]-1 >= 0 else None,
+			Node.SOUTH : (pos[0]+1, pos[1]) if pos[0]+1 < Board.SIZE else None,
+			Node.EAST  : (pos[0], pos[1]+1) if pos[1]+1 < Board.SIZE else None,
+			Node.WEST  : (pos[0], pos[1]-1) if pos[1]-1 >= 0 else None
+		}
+		n = self.grid[pos[0]][pos[1]]
+		retlist = []
+		for direction, npos in neighbors.iteritems():
+			if npos and not n.has_wall(direction):
+				retlist.append(self.grid[npos[0]][npos[1]])
+		return retlist
+
+	def path(self, start, goals):
+		from Queue import Queue
+		q = Queue()
+		for g in goals:
+			q.put(g)
+
+
+	def can_step(self, posA, posB):
+		"""Check whether or not (adjacent) positions posA and PosB are connected"""
+		ra, ca = posA
+		rb, cb = posB
+		# first assert adjacency - row diff + col diff must be 1
+		if abs(ra-rb) + abs(ca-cb) != 1:
+			return False
+		elif ra > rb:
+			return not (self.grid[ra][ca].has_wall(Node.NORTH))
+		elif ra < rb:
+			return not (self.grid[ra][ca].has_wall(Node.SOUTH))
+		elif ca > cb:
+			return not (self.grid[ra][ca].has_wall(Node.WEST))
+		elif ca < cb:
+			return not (self.grid[ra][ca].has_wall(Node.EAST))
 
 	@classmethod
 	def __create_grid(cls):
 		"""
 		The grid consists of a SIZE x SIZE array of nodes indexed by [row][column]. 
-		Each node is connected to its immediate neighbors.
 		"""
 		# Create Nodes
 		g = [None] * Board.SIZE
 		for r in range(Board.SIZE):
-			g[r] = [Node() for c in range(Board.SIZE)]
-		# Connect node neighbors
-		# could be faster, but it is only done at initialization and the board is small
-		for r in range(Board.SIZE):
-			for c in range(Board.SIZE):
-				for offr, offc in [(-1,0), (1,0), (0,-1), (0,1)]:
-					if r + offr in range(Board.SIZE) and c + offc in range(Board.SIZE):
-						g[r][c].connect(g[r+offr][c+offc])
+			g[r] = [Node(r, c) for c in range(Board.SIZE)]
+		return g
 
