c71d950fdb1823ca0bb9182bc3129604589de3eb
wrongu <richard.d.lange.13@dartmouth.edu>
2014-02-18 15:42:30 -0500

    Added notation docs, some player adding, test code

--- Quoridor.py
+++ Quoridor.py
@@ -1,6 +1,7 @@
 # Quoridor.py
 #
-# This file is the top-level API for a single game of Quoridor
+# This contains the Quoridor class (the state-machine representation of a single game of Quoridor), with supporting
+# functions, exceptions, and classes
 #
 # Author: wrongu
 # Date: February 2014
@@ -12,6 +13,12 @@ class State:
 	INIT = 0
 	PLAYING = 1
 	OVER = 2
+
+class StateError(Exception):
+	pass
+
+class IllegalMove(Exception):
+	pass
 
 def require_state(st):
 	'''A decorator method for requiring certain state
@@ -27,6 +34,32 @@ def require_state(st):
 	return decorator
 
 class Quoridor(object):
+	'''A class representing a single game (2 or 4 player) of Quoridor.
+
+	NOTATION:
+		While grid locations are represented as a tuple of two numbers, the actual game notation uses letters for columns. 1-9 becomes a-i        
+			(1,1) --> 1a
+			(2,7) --> 2g
+			(9,9) --> 9i
+		
+		A *move* is denoted by the notated-form of the destination. Moving from 3b to 3c is just "3c"
+		A *wall* is denoted with a 3-character string. The first character is "H" or "V" for Horizontal or Vertical walls
+			-horizontal walls lie between rows and span 2 columns
+			-vertical walls lie between columns and span 2 rows
+
+			the other 2 characters specify the point of the top-left corner of the wall (lowest row, lowest col)
+			- must be between (1,1) and (8,8), or 1a and 8h
+		
+		visuals! (I suggest viewing with a fixed-width font)
+		horizontal  vertical
+		A  B        A || B
+		====          ||
+		C  D        C || D
+		
+		for both of these, the point "A" will be used to denote the wall's location. For example, a fully notated wall
+		might be 'H4d' for a horizontal wall that touches 4d, 5d, 4e, and 5e
+
+	'''
 	state = State.INIT
 	board = None
 	players = []
@@ -35,26 +68,64 @@ class Quoridor(object):
 
 	def __init__(self):
 		self.board = Board()
+		self.current_player = -1
 
 	@require_state(State.INIT)
-	def add_player(self, player):
-		if isinstance(player, Player) and len(self.players) < 4:
-			self.players.append(player)
+	def create_player(self, pname):
+		'''If game is not already full, add the specified player and return an id
+
+		An id of -1 means the game is full
+		'''
+		if len(self.players) < 4:
+			self.players.append(Player(name))
+			return len(self.players)
 		else:
-			raise Exception("Cannot add a 5th player: %s" % str(player))
+			return -1
 
 	@require_state(State.INIT)
 	def begin_game(self):
-		# assert player count
-		# choose starting player
-		self.state = State.PLAYING
+		'''Start the game (move from INIT to PLAYING)
+
+		An exception is raised if there are not 2 or 4 players
+		'''
+		if len(self.players) == 2 or len(self.players) == 4:
+			self.current_player = 0
+			self.state = State.PLAYING
+		else:
+			raise StateError("Cannot start a game with %d players" % len(self.players))
+
+	def __try_turn(self, turn):
+		if len(turn) == 2:
+			# MOVE
+			pass
+		elif len(turn) == 3:
+			# WALL
+			pass
+
+	@require_state(State.PLAYING)
+	def do_turn(self, pid, turn):
+		'''Attempt the given turn for the given player. If it is not that player's turn, a StateError is raised.
+
+		A turn is either Moving or placing a Wall. The notation is as follows:
+
+		# Moving: each row is 
+
+		# Walls:
+		'''
+		if pid == self.current_player:
+			try:
+				self.__try_turn(turn)
+			except IllegalMove as e:
+				print "Illegal Move by %d: %s" % (pid, str(turn))
+		else:
+			raise StateError("Bad request for a turn by player %d. It's %d's turn." % (pid, self.current_player))
 
 if __name__ == '__main__':
 	# Testing...
 	q = Quoridor()
-	q.add_player(Player('A'))
-	q.add_player(Player('B'))
-	q.add_player(Player('C'))
-	q.add_player(Player('D'))
+	q.create_player(Player('A'))
+	q.create_player(Player('B'))
+	q.create_player(Player('C'))
+	q.create_player(Player('D'))
 	q.begin_game()
 	q.begin_game()
