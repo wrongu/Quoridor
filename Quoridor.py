76ad6c6c6964ff007fdf30c25d90dfd7c2ebb435
wrongu <lange.richard.d@gmail.com>
2014-02-26 12:45:20 -0500

    added jumping; updated comments for new notation

--- Quoridor.py
+++ Quoridor.py
@@ -37,18 +37,19 @@ class Quoridor(object):
 	'''A class representing a single game (2 or 4 player) of Quoridor.
 
 	NOTATION:
-		While grid locations are represented as a tuple of two numbers, the actual game notation uses letters for columns. 1-9 becomes a-i        
-			(1,1) --> 1a
-			(2,7) --> 2g
-			(9,9) --> 9i
+		While grid locations are represented as a tuple of two zero-indexed numbers, the actual game notation 
+		uses letters for rows. 1-9 becomes a-i        
+			(0,0) --> a1
+			(1,6) --> g2
+			(8,8) --> i9
 		
-		A *move* is denoted by the notated-form of the destination. Moving from 3b to 3c is just "3c"
+		A *move* is denoted by the notated-form of the destination. Moving from b3 to c3 is just "c3"
 		A *wall* is denoted with a 3-character string. The first character is "H" or "V" for Horizontal or Vertical walls
 			-horizontal walls lie between rows and span 2 columns
 			-vertical walls lie between columns and span 2 rows
 
 			the other 2 characters specify the point of the top-left corner of the wall (lowest row, lowest col)
-			- must be between (1,1) and (8,8), or 1a and 8h
+			- must be between (0,0) and (7,7), or a1 and h8
 		
 		visuals! (I suggest viewing with a fixed-width font)
 		horizontal  vertical
@@ -57,7 +58,7 @@ class Quoridor(object):
 		C  D        C || D
 		
 		for both of these, the point "A" will be used to denote the wall's location. For example, a fully notated wall
-		might be 'H4d' for a horizontal wall that touches 4d, 5d, 4e, and 5e
+		might be 'd4h' for a horizontal wall that touches d4, d5, e4, and e5
 
 	'''
 	state = State.INIT
@@ -67,22 +68,22 @@ class Quoridor(object):
 	future = []  # stack of 'future moves' (i.e. the redo stack). future[-1] is the next move, future[0] is farthest in the future
 	self.played_walls = []
 
-	ALL_WALLS = ['H1a', 'H1b', 'H1c', 'H1d', 'H1e', 'H1f', 'H1g', 'H1h',
-				'H2a', 'H2b', 'H2c', 'H2d', 'H2e', 'H2f', 'H2g', 'H2h',
-				'H3a', 'H3b', 'H3c', 'H3d', 'H3e', 'H3f', 'H3g', 'H3h',
-				'H4a', 'H4b', 'H4c', 'H4d', 'H4e', 'H4f', 'H4g', 'H4h',
-				'H5a', 'H5b', 'H5c', 'H5d', 'H5e', 'H5f', 'H5g', 'H5h',
-				'H6a', 'H6b', 'H6c', 'H6d', 'H6e', 'H6f', 'H6g', 'H6h',
-				'H7a', 'H7b', 'H7c', 'H7d', 'H7e', 'H7f', 'H7g', 'H7h',
-				'H8a', 'H8b', 'H8c', 'H8d', 'H8e', 'H8f', 'H8g', 'H8h',
-				'V1a', 'V1b', 'V1c', 'V1d', 'V1e', 'V1f', 'V1g', 'V1h',
-				'V2a', 'V2b', 'V2c', 'V2d', 'V2e', 'V2f', 'V2g', 'V2h',
-				'V3a', 'V3b', 'V3c', 'V3d', 'V3e', 'V3f', 'V3g', 'V3h',
-				'V4a', 'V4b', 'V4c', 'V4d', 'V4e', 'V4f', 'V4g', 'V4h',
-				'V5a', 'V5b', 'V5c', 'V5d', 'V5e', 'V5f', 'V5g', 'V5h',
-				'V6a', 'V6b', 'V6c', 'V6d', 'V6e', 'V6f', 'V6g', 'V6h',
-				'V7a', 'V7b', 'V7c', 'V7d', 'V7e', 'V7f', 'V7g', 'V7h',
-				'V8a', 'V8b', 'V8c', 'V8d', 'V8e', 'V8f', 'V8g', 'V8h']
+	ALL_WALLS = ['a1h', 'b1h', 'c1h', 'd1h', 'e1h', 'f1h', 'g1h', 'h1h',
+				'a2h', 'b2h', 'c2h', 'd2h', 'e2h', 'f2h', 'g2h', 'h2h',
+				'a3h', 'b3h', 'c3h', 'd3h', 'e3h', 'f3h', 'g3h', 'h3h',
+				'a4h', 'b4h', 'c4h', 'd4h', 'e4h', 'f4h', 'g4h', 'h4h',
+				'a5h', 'b5h', 'c5h', 'd5h', 'e5h', 'f5h', 'g5h', 'h5h',
+				'a6h', 'b6h', 'c6h', 'd6h', 'e6h', 'f6h', 'g6h', 'h6h',
+				'a7h', 'b7h', 'c7h', 'd7h', 'e7h', 'f7h', 'g7h', 'h7h',
+				'a8h', 'b8h', 'c8h', 'd8h', 'e8h', 'f8h', 'g8h', 'h8h',
+				'a1v', 'b1v', 'c1v', 'd1v', 'e1v', 'f1v', 'g1v', 'h1v',
+				'a2v', 'b2v', 'c2v', 'd2v', 'e2v', 'f2v', 'g2v', 'h2v',
+				'a3v', 'b3v', 'c3v', 'd3v', 'e3v', 'f3v', 'g3v', 'h3v',
+				'a4v', 'b4v', 'c4v', 'd4v', 'e4v', 'f4v', 'g4v', 'h4v',
+				'a5v', 'b5v', 'c5v', 'd5v', 'e5v', 'f5v', 'g5v', 'h5v',
+				'a6v', 'b6v', 'c6v', 'd6v', 'e6v', 'f6v', 'g6v', 'h6v',
+				'a7v', 'b7v', 'c7v', 'd7v', 'e7v', 'f7v', 'g7v', 'h7v',
+				'a8v', 'b8v', 'c8v', 'd8v', 'e8v', 'f8v', 'g8v', 'h8v']
 
 	def __init__(self, n_players=2):
 		if n_players != 2 and n_players != 4:
@@ -90,6 +91,8 @@ class Quoridor(object):
 		self.board = Board()
 		self.current_player = -1
 		self.n_players = n_players
+		self.players = []
+		self.player_moves = [] # player_moves[id] is an array of tuples where that player may move to, updated at the start of their turn
 
 	@require_state(State.INIT)
 	def create_player(self, pname):
@@ -129,26 +132,32 @@ class Quoridor(object):
 			raise StateError("Cannot start a game with %d players" % len(self.players))
 
 	@require_state(State.PLAYING)
-	def do_turn(self, pid, turn):
+	def do_turn(self, pid, turnstring):
 		'''Attempt the given turn for the given player. If it is not that player's turn, a StateError is raised.
 
 		A turn is either Moving or placing a Wall. The notation is as follows:
-
-		# Moving: each row is 
-
-		# Walls:
 		'''
 		if pid == self.current_player:
-			try:
-				# try executing the turn
-				self.__try_turn(turn)
-				# if no exception, it was successful.
-				self.current_player = (self.current_player + 1) % len(self.players)
-				# (game could be over though)
+			legal, info = self.turn_is_legal(turnstring)
+			if legal:
+				if isinstance(info, Node):
+					self.__current_player().update_position(info.position)
+				elif isinstance(info, Wall):
+					self.board.add_wall(info)
+				else:
+					raise StateError("broken internal function turn_is_legal")
+				# game could be over
 				if self.__game_is_over():
 					self.state = State.OVER
-			except IllegalMove as e:
-				print "Illegal Move by %d: %s" % (pid, str(turn))
+					# note that by changing the state, no futher moves may be made, and "current_player"
+					# is now the winning player
+				else:
+					# advance to next player
+					self.current_player = (self.current_player + 1) % len(self.players)
+					# update moves now that it's a new player's turn
+					self.__prep_moves(self.current_player)
+			else:
+				raise IllegalMove(info)
 		else:
 			raise StateError("Bad request for a turn by player %d. It's %d's turn." % (pid, self.current_player))
 
@@ -156,10 +165,11 @@ class Quoridor(object):
 	def turn_is_legal(self, turnstring):
 		if len(turnstring) == 2:
 			# MOVE
-			newpos = Node.parse(turnstring)
-			playerpos = self.__current_player().get_position()
-			if self.board.can_step(playerpos, newpos):
-				return (True, "") # player may move to the given position
+			# self.player_moves is updated at the start of this turn with all locations where the current
+			# player may move.. so this is a simple lookup
+			n = Node.parse(turnstring)
+			if n.position in self.player_moves[self.current_player]:
+				return (True, n)
 			else:
 				return (False, "%s cannot move to %s" % (str(self.__current_player()), turnstring))
 		elif len(turnstring) == 3:
@@ -185,7 +195,7 @@ class Quoridor(object):
 			self.board.remove_wall(w)
 
 			# all checks passed!
-			return (True, "")
+			return (True, w)
 		else:
 			return (False, "%s is not a valid turn string" % turnstring)
 
@@ -197,6 +207,32 @@ class Quoridor(object):
 	def redo(self):
 		pass # TODO
 
+	@require_state(State.PLAYING)
+	def __prep_moves(self, pid):
+		pl = self.players[pid]
+		(pr, pc) = pl.position
+		# get all adjacent squares (0 to 4 of them)
+		adjacents = set(self.board.neighbors(pl.position))
+		players_pos = set([p.position for p in self.players])
+		# free to step to any adjacent squares that do not have players on them
+		steppable = list(adjacents - players_pos)
+		# further processing required if player is adjacent - pl can jump
+		jumps = adjacents & players_pos
+		for (jr, jc) in jumps:
+			# need to know direction we are going
+			(dir_r, dir_c) = (jr-pr, jc-pc)
+			# simple jump: over other player
+			simple_next = (jr + dir_r, jc+dir_c)
+			# test if simple jump is ok (i.e. no wall and no 3rd player there)
+			if self.board.can_step((jr,jc), simple_next) and simple_next not in players_pos:
+					steppable.append(simple_next)
+			else:
+				# jump is blocked either by a wall or by a 3rd player. this is where diagonal moves are allowed
+				two_away = (simple_next[0] + dir_r, simple_next[1] + dir_c)
+				diagonals_set = set(self.board.neighbors(simple_next)) - set([pl.position, two_away])
+				steppable.extend(list(diagonals_set))
+		return steppable
+ 
 	@require_state(State.PLAYING)
 	def __current_player(self):
 		return self.players[self.current_player]
