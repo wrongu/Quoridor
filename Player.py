6fc7d6880e049d06042910577fb2feebb5a3b45e
wrongu <lange.richard.d@gmail.com>
2014-02-26 13:12:17 -0500

    added walls to Player, made members private

--- Player.py
+++ Player.py
@@ -2,21 +2,39 @@
 #
 # The Player class represents a generic player (human, ai, networked, etc)
 
+from Quoridor import StateError
+
 class Player(object):
 
-	def __init__(self, name, start_position, goals):
-		self.name = name
-		self.position = start_position
-		self.goals = goals
+	def __init__(self, name, start_position, goals, start_walls):
+		self.__name = name
+		self.__position = start_position
+		self.__goals = goals
+		self.__wall_count = start_walls
 
 	def __str__(self):
-		return str(name)
+		return str(self.__name)
 
 	def get_position(self):
-		return self.position
+		return self.__position
 
 	def update_position(self, newpos):
-		self.position = newpos
+		self.__position = newpos
 
 	def reached_goal(self):
-		return self.position in self.goals
+		return self.__position in self.__goals
+
+	def num_walls():
+		return self.__wall_count
+
+	def position():
+		return self.__position
+
+	def use_wall():
+		if self.__wall_count > 0:
+			self.__wall_count -= 1
+		else:
+			raise StateError("Player %s has no walls remaining" % self.__name)
+
+	def unuse_wall():
+		self.__wall_count += 1
