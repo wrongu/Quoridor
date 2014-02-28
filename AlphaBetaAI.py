bd7c622a5b190f8decef22252540344a02d1a318
wrongu <lange.richard.d@gmail.com>
2014-02-28 17:22:35 -0500

    AI handles game over. (No longer in thread.. debugging)

--- AlphaBetaAI.py
+++ AlphaBetaAI.py
@@ -9,6 +9,7 @@ class AlphaBetaAI2P(object):
 		self.processing = False
 		self.leaves = 0
 		self.thread = None
+		self.result = ''
 
 	def kill(self):
 		if self.thread and self.processing:
@@ -17,23 +18,33 @@ class AlphaBetaAI2P(object):
 	def is_processing(self):
 		return self.processing
 
-	def process(self, depth):
+	def process(self, depth, callback=None):
 		"""Start processing for turn in a separate thread"""
 		# processing acts as a lock
 		if not self.processing:
-			self.thread = Thread(target=self.__alpha_beta_top, args = (self.game.copy(), self.player, depth))
-			self.thread.daemon = True
-			self.thread.start()
+			# self.thread = Thread(target=self.__alpha_beta_top, args = (self.game.copy(), self.player, depth, callback))
+			# self.thread.daemon = True
+			# self.thread.start()
+			self.__alpha_beta_top(self.game.copy(), self.player, depth, callback)
+		else:
+			print "AI already in use"
 
 	def __eval(self, state):
+		score = 0
+		# game-over is kind of a big deal
+		if state.game_is_over():
+			score += (50 if state.get_current_pid() == self.player else -50)
 		pl = state.get_player(self.player)
 		# length of shortest path is opposite to how good this state is.
-		selfish = len(state.get_board().path(pl.position(), pl.goals()))
-		# similarly, state is worst if other players are doing well
-		cautious = min([len(state.get_board().path(p.position(), p.goals())) for p in state.others(self.player)])
-		return selfish + cautious
+		score -= len(state.get_board().path(pl.position(), pl.goals()))
+		# similarly, state is better if the best of other players is doing poorly
+		score += min([len(state.get_board().path(p.position(), p.goals())) for p in state.others(self.player)])
+		# hoarding walls is good, right?
+		# TODO weight walls and paths separately
+		score += pl.num_walls()
+		return score
 
-	def __alpha_beta_top(self, state_copy, player, depthlimit):
+	def __alpha_beta_top(self, state_copy, player, depthlimit, callback):
 		self.processing = True
 		self.leaves = 0
 		tstart = time.time()
@@ -43,17 +54,20 @@ class AlphaBetaAI2P(object):
 		print "STATS (depth %d):" % depthlimit
 		print "\t%d leaf states visited" % self.leaves
 		print "\t%f seconds total" % telapse
-		self.game.do_turn(self.player, turn)
 		self.processing = False
+		self.result = turn
+		if callback:
+			callback(turn)
 
 	def __alpha_beta(self, depth, state, minmax=1, alpha=-10000, beta=10000):
 		# base case
-		if depth == 0:
+		if depth == 0 or state.game_is_over():
 			self.leaves += 1
 			return ('', self.__eval(state))
 		best = ''
 		# recursive case
 		for turn in state.all_turns():
+			# print "%s%d: turn %s" % (''.join(['  ']*depth), depth, turn)
 			# copy state for next level, and perform the turn
 			scopy = state.copy()
 			 # TODO fix the hack:
