7f0e2054e1f6ae6bfc03a6280d4c722da15530c1
wrongu <richard.d.lange.13@dartmouth.edu>
2014-07-19 08:13:18 -0400

    minor refactor to profile_ai

--- profile_ai.py
+++ profile_ai.py
@@ -11,13 +11,19 @@ def newgame(p1="1P", p2="2P"):
 	g.begin_game()
 	return g
 
-stats = None
-if __name__ == '__main__':
+def runprofile(depth=1):
+	stats = None
 	g = newgame()
 	ai = AI(g, 0)
-	depth = 1
 	fname ='tests/stats/AIProfile.dat'
-	cProfile.run('ai.process(%d)' % depth, fname)
+	cProfile.runctx('ai.process(%d)' % depth, globals(), {'ai': ai}, fname)
 	stats = pstats.Stats(fname)
 	stats.sort_stats('tottime')
 	stats.print_stats()
+
+if __name__ == '__main__':
+	from sys import argv
+	if len(argv) > 1:
+		runprofile(int(argv[1]))
+	else:
+		runprofile()
